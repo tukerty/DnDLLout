c3466a67c08afd35407164fb1df77a659a6b2599
ptsh <atah-heroes@yandex.ru>
2018-04-15 14:04:14 +0500

    inputText

---
+++ bin/event-system.js
@@ -0,0 +1,6 @@
+
+var inputText =document.getElementById("text_field").value;
+    function getInputText(){
+        inputText=document.getElementById("text_field").value;
+        console.log(inputText);
+    }
