d99cb6598441a60a38b8cb9fd817f625629b5d87
ptsh <atah-heroes@yandex.ru>
2018-04-15 14:47:04 +0500

    eventSystemDipatcherUpdate

--- bin/event-system.js
+++ bin/event-system.js
@@ -1,7 +1,9 @@
 document.addEventListener('renderComplete', function (e) {
-    document.getElementById('submit').addEventListener('click', function(e){
+    document.getElementById('message_dispatcher').addEventListener('submit', function(e){
+        e.preventDefault();
         inputText=document.getElementById('text_field').value;
         log.verbose('ive just sent u some data: ' + inputText)
+        document.getElementById('text_field').value="";
     })
 });
 
